b728027ea6403200f49cddb2cc427c01115645c2
jihaduttolibinn <jihaduttolibin02@gmail.com>
2025-11-20 15:45:12 +0700

    Menambahkan Data.kt

---
+++ app/src/main/java/com/example/arsitekturmvvm/model/DataJK.kt
@@ -0,0 +1,11 @@
+package com.example.arsitekturmvvm.model
+
+
+import com.example.arsitekturmvvm.R
+
+object DataJK {
+    val Jenisk = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
