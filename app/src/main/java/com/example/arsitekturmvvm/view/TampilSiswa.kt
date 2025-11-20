45c926cb0ec21b316cf895edfd1079fec6ce7c11
jihaduttolibinn <jihaduttolibin02@gmail.com>
2025-11-20 16:56:38 +0700

    Untuk  Codingan TampilSiswa.kt selesai

--- app/src/main/java/com/example/arsitekturmvvm/view/TampilSiswa.kt
+++ app/src/main/java/com/example/arsitekturmvvm/view/TampilSiswa.kt
@@ -3,11 +3,11 @@ package com.example.arsitekturmvvm.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
@@ -31,10 +31,53 @@ import com.example.arsitekturmvvm.model.Siswa
 @Composable
 fun TampilSiswa(
     statusUiSiswa: Siswa,
-    onBackBtnClick:()-> Unit
+    onBackButtonClicked:()->Unit
 ){
-    val  items = listOf(
-        Pair(first = stringResource(id = R.string.nama), statusUiSiswa.nama),
-        Pair(first = stringResource(id = R.string.gender), statusUiSiswa.gender),
-        Pair(first = stringResource(id = R.string.alamat),statusUiSiswa.alamat),
+    val items = listOf(
+        Pair(first = stringResource(id = R.string.nama), second = statusUiSiswa.nama),
+        Pair(first = stringResource(id = R.string.gender), second = statusUiSiswa.gender),
+        Pair(first = stringResource(id = R.string.alamat), second = statusUiSiswa.alamat)
     )
+    Scaffold (modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id=R.string.detail),
+                        color = Color.White
+                    )},
+                colors = TopAppBarDefaults
+                    .mediumTopAppBarColors(colorResource
+                        (id = R.color.purple_500)
+                    )
+            )
+        }){     isiRuang->
+        Column(
+            modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column(
+                modifier = Modifier.padding(dimensionResource(id=R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.spacedBy(dimensionResource
+                    (R.dimen.padding_small))
+            ) {
+                items.forEach { item ->
+                    Column {
+                        Text(text = item.first.uppercase(), fontSize = 16.sp)
+                        Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_small)))
+                        Text(text = item.second, fontWeight = FontWeight.Bold, fontFamily = FontFamily.Cursive, fontSize = 16.sp)
+                    }
+                    Divider(thickness = dimensionResource( R.dimen.thickness_divider))
+                }
+                Spacer(modifier = Modifier.height(10.dp))
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = onBackButtonClicked
+                ) {
+                    Text(text = stringResource(id=R.string.back))
+                }
+            }
+        }
+
+    }
+}
